422ca8211bc8f0dab20b834b0d43d819876e35e5
ddenyer <denyer.d@gmail.com>
2026-03-16 19:58:41 +0000

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -5,19 +5,19 @@ import StatTool from './tools/stat/StatTool'
 import DefineTool from './tools/define/DefineTool'
 
 function App() {
-    return (
-          <BrowserRouter>
-                <Routes>
-                        <Route path="/stat" element={<StatTool />} />
-                        <Route path="/define" element={<DefineTool />} />
-                        <Route path="/" element={<div style={{ padding: '2rem' }}><h1>ChangeBefore Tools</h1>h1></div>div>} />
-                        </Route>Routes>
-                </Routes>BrowserRouter>
-            )
-            }
-          
-          ReactDOM.createRoot(document.getElementById('root')).render(
-            <React.StrictMode>
-                <App />
-            </React.StrictMode>React.StrictMode>,
-          )</BrowserRouter>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/stat" element={<StatTool />} />
+        <Route path="/define" element={<DefineTool />} />
+        <Route path="/" element={<div style={{ padding: '2rem' }}><h1>ChangeBefore Tools</h1></div>} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
